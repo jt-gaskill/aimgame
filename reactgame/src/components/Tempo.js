da7ad8b63a0d20eba67965fa359e5d7541c9b0b4
Jonah Gaskill <jonahgaskill@gmail.com>
2022-06-12 23:40:11 -0500

    tempo boxes spawning and despawning

--- reactgame/src/components/Tempo.js
+++ reactgame/src/components/Tempo.js
@@ -1,23 +1,34 @@
 import React from "react"
 
 export default function Tempo(props){
-    const [boxes, setBoxes] = React.useState(30)
+    const [boxes, setBoxes] = React.useState([{direction: "up", right: 30}])
     const [bordercolor, setBorderColor] = React.useState("red")
 
-    console.log(boxes)
     React.useEffect(() => {
-        if(boxes<790){
-            let interval = null
-            interval = setInterval(() => setBoxes(boxes + 1), 5)
-            return () => clearInterval(interval)
-        }
-        
+        let interval = null
+        interval = setInterval(() => {
+            const tempboxes = boxes.map((box) => ({...box, right: box.right+1}))
+            if(tempboxes[0].right > 750){
+                tempboxes.shift()
+            }
+            if(Math.random() < .008){
+                let newdirec = Math.random()
+                if(newdirec < .25){newdirec = "up"}
+                else if(newdirec < .5){newdirec = "down"}
+                else if(newdirec < .75){newdirec = "left"}
+                else{newdirec = "right"}
+
+             tempboxes.push({direction: newdirec, right: 30})
+            }
+            setBoxes(tempboxes)
+        }, 5)
+        return () => clearInterval(interval)
     },[boxes])
 
     document.addEventListener("keydown", function handler(event){
         console.log(event.key)
         if(event.key === "ArrowUp"){
-            if(boxes>360 && boxes<400){
+            if(boxes[0].right>360 && boxes[0].right<400){
                 setBorderColor("green")
             }else{
                 setBorderColor("red")
@@ -26,13 +37,14 @@ export default function Tempo(props){
         this.removeEventListener('keydown', handler)
     })
 
+    // const boxdivs = <div className={"w-10 h-10 absolute top-[230px] text-black"} style={{right: boxes[0].right, backgroundColor: "red"}}>{boxes[0].direction}</div>
+    const boxdivs = boxes.map((box) => <div className={"w-10 h-10 absolute top-[230px] text-black"} style={{right: box.right, backgroundColor: "red"}}>{box.direction}</div>)
+
     return (
         <div className="h-[500px] w-[800px] bg-black relative text-white select-none overflow-hidden flex">
-            <div className="border-white h-20 w-20 border-2 my-auto mx-auto" style={{borderColor: bordercolor}}>
+            <div className="border-white h-20 w-20 border-2 my-auto mx-auto" style={{borderColor: bordercolor}}></div>
 
-            </div>
-            <div className={"w-10 h-10 absolute top-[230px] text-black"} style={{right: boxes, backgroundColor: "red"}}>up</div>
-            {/* <div className={pos}></div> */}
+            {boxdivs}
             
         </div>
     )
