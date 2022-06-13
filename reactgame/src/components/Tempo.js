1f30d362ec477d64e5a161f42ed9425b2d711bc4
Jonah Gaskill <jonahgaskill@gmail.com>
2022-06-13 10:56:04 -0500

    hopefully finished tempo

--- reactgame/src/components/Tempo.js
+++ reactgame/src/components/Tempo.js
@@ -1,4 +1,5 @@
 import React from "react"
+import {BsArrowLeft, BsArrowDown, BsArrowUp, BsArrowRight} from "react-icons/bs"
 
 export default function Tempo(props){
     const [boxes, setBoxes] = React.useState([{direction: "up", right: 30}])
@@ -15,7 +16,10 @@ export default function Tempo(props){
             
             if(tempboxes[0] && tempboxes[0].right > 400){
                 tempmissed.push(tempboxes.shift())
-                props.handleCount(props.count - 1)
+                if(props.active){
+                    props.handleCount(props.count - 1)
+                }
+                
                 
             }
             setMissed(tempmissed)
@@ -29,82 +33,104 @@ export default function Tempo(props){
              tempboxes.push({direction: newdirec, right: 30})
             }
             setBoxes(tempboxes)
-        }, 5)
+        }, 4)
         return () => clearInterval(interval)
     },[boxes])
 
+    React.useEffect(() => {
+        if(props.active){
+            setBoxes([{direction: "up", right: 30}])
+            setMissed([])
+        }
+    } ,[props.active])
+
     document.addEventListener("keydown", function handler(event){
         console.log(event.key)
-        if(boxes[0] && boxes[0].right>360 && boxes[0].right<400){
-            if(event.key == "ArrowUp"){
-                if(boxes[0] && boxes[0].direction === "up"){
-                    props.handleCount(props.count + 1)
-                    let tempboxes = boxes
-                    tempboxes.shift()
-                    setBoxes(tempboxes)
+        if(props.active){
+            if(boxes[0] && boxes[0].right>360 && boxes[0].right<400){
+                if(event.key === "ArrowUp"){
+                    if(boxes[0] && boxes[0].direction === "up"){
+                        props.handleCount(props.count + 1)
+                        let tempboxes = boxes
+                        tempboxes.shift()
+                        setBoxes(tempboxes)
+                    }
+                    else{
+                        props.handleCount(props.count -1)
+                    }
+                }
+                else if(event.key === "ArrowLeft"){
+                    if(boxes[0] && boxes[0].direction === "left"){
+                        props.handleCount(props.count + 1)
+                        let tempboxes = boxes
+                        tempboxes.shift()
+                        setBoxes(tempboxes)
+                    }
+                    else{
+                        props.handleCount(props.count -1)
+                    }
+                }
+                else if(event.key === "ArrowRight"){
+                    if(boxes[0] && boxes[0].direction === "right"){
+                        props.handleCount(props.count + 1)
+                        let tempboxes = boxes
+                        tempboxes.shift()
+                        setBoxes(tempboxes)
+                    }
+                    else{
+                        props.handleCount(props.count -1)
+                    }
+                }
+                else if(event.key === "ArrowDown"){
+                    if(boxes[0] && boxes[0].direction === "down"){
+                        props.handleCount(props.count + 1)
+                        let tempboxes = boxes
+                        tempboxes.shift()
+                        setBoxes(tempboxes)
+                    }
+                    else{
+                        props.handleCount(props.count -1)
+                    }
                 }
             }
-            else if(event.key == "ArrowLeft"){
-                if(boxes[0] && boxes[0].direction === "left"){
-                    props.handleCount(props.count + 1)
-                    let tempboxes = boxes
-                    tempboxes.shift()
-                    setBoxes(tempboxes)
-                }
-            }
-            else if(event.key == "ArrowRight"){
-                if(boxes[0] && boxes[0].direction === "right"){
-                    props.handleCount(props.count + 1)
-                    let tempboxes = boxes
-                    tempboxes.shift()
-                    setBoxes(tempboxes)
-                }
-            }
-            else if(event.key == "ArrowDown"){
-                if(boxes[0] && boxes[0].direction === "down"){
-                    props.handleCount(props.count + 1)
-                    let tempboxes = boxes
-                    tempboxes.shift()
-                    setBoxes(tempboxes)
-                }
-            }
-            
         }
         this.removeEventListener('keydown', handler)
     })
 
-    // const boxdivs = <div className={"w-10 h-10 absolute top-[230px] text-black"} style={{right: boxes[0].right, backgroundColor: "red"}}>{boxes[0].direction}</div>
     const boxdivs = boxes.map((box) => {
         let offset = {right: 230}
-        if(box.direction === "up"){offset = {top: "88px", backgroundColor: "blue"}}
-        else if(box.direction === "left"){offset = {top: "184px", backgroundColor: "red"}}
-        else if(box.direction === "right"){offset = {top: "280px", backgroundColor: "green"}}
-        else if(box.direction === "down"){offset = {top: "376px", backgroundColor: "yellow"}}
-        return <div className={"w-10 h-10 absolute text-black"} style={{right: box.right, ...offset}}>{box.direction}</div>
+        if(box.direction === "up"){offset = {top: "88px", backgroundColor: "blue", icon: <BsArrowUp />, paddingTop: "5px"}}
+        else if(box.direction === "left"){offset = {top: "184px", backgroundColor: "red", icon: <BsArrowLeft />, paddingTop: "5px"}}
+        else if(box.direction === "right"){offset = {top: "280px", backgroundColor: "green", icon: <BsArrowRight />, paddingTop: "5px"}}
+        else if(box.direction === "down"){offset = {top: "376px", backgroundColor: "yellow", icon: <BsArrowDown />, paddingTop: "5px"}}
+        return <div className={"w-10 h-10 absolute text-black flex justify-center text-3xl"} style={{right: box.right, ...offset}}>{offset.icon}</div>
     })
 
     const misseddivs = missed.map((box) => {
         let offset = {right: 230}
-        if(box.direction === "up"){offset = {top: "88px", backgroundColor: "blue"}}
-        else if(box.direction === "left"){offset = {top: "184px", backgroundColor: "red"}}
-        else if(box.direction === "right"){offset = {top: "280px", backgroundColor: "green"}}
-        else if(box.direction === "down"){offset = {top: "376px", backgroundColor: "yellow"}}
-        return <div className={"w-10 h-10 absolute text-black"} style={{right: box.right, ...offset}}>{box.direction}</div>
+        if(box.direction === "up"){offset = {top: "88px", backgroundColor: "blue", icon: <BsArrowUp />, paddingTop: "5px"}}
+        else if(box.direction === "left"){offset = {top: "184px", backgroundColor: "red", icon: <BsArrowLeft />, paddingTop: "5px"}}
+        else if(box.direction === "right"){offset = {top: "280px", backgroundColor: "green", icon: <BsArrowRight />, paddingTop: "5px"}}
+        else if(box.direction === "down"){offset = {top: "376px", backgroundColor: "yellow", icon: <BsArrowDown />, paddingTop: "5px"}}
+        return <div className={"w-10 h-10 absolute text-black flex justify-center text-3xl"} style={{right: box.right, ...offset}}>{offset.icon}</div>
     })
     // 230px
     return (
-        <div className="h-[500px] w-[800px] bg-black relative text-white select-none overflow-hidden flex">
+        
+        <div className="h-[500px] w-[800px] bg-black flex justify-center select-none">
+            {props.active ? 
+            <div className="h-[500px] w-[800px] bg-black relative text-white select-none overflow-hidden flex">
             <div className="mx-auto flex flex-col justify-center">
                 <div className="border-white h-20 w-20 border-2 my-2" style={{borderColor: "blue"}}></div>
                 <div className="border-white h-20 w-20 border-2 my-2" style={{borderColor: "red"}}></div>
                 <div className="border-white h-20 w-20 border-2 my-2" style={{borderColor: "green"}}></div>
                 <div className="border-white h-20 w-20 border-2 my-2" style={{borderColor: "yellow"}}></div>
             </div>
-            
-
             {boxdivs}
             {misseddivs}
             
+            </div>
+            : <p className="text-white leading-[500px]">Don't miss a box</p>}
         </div>
     )
 }
